5da2e6144a57ab78384c8bd1d5eacef42b5ee39f
Matthew Mbonu <mcmbonu1@gmail.com>
2019-05-10 13:08:55 -0400

    heroku-setup

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
   console.log("server running");
 });
